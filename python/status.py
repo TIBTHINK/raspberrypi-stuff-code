4b008f7cdd8440e21f03cc381b21f329d64b2f68
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-10-25 12:26:29 -0400

    Update status.py

--- python/status.py
+++ python/status.py
@@ -3,9 +3,6 @@ from time import sleep
 while True:
     f = open("/sys/class/thermal/thermal_zone0/temp", "r")
     t = f.readline ()
-
-    c = float(t/1000)
-
     cputemp = "CPU temp: "+t
     sleep(1)
     print(cputemp)
