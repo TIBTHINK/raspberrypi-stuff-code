e8d4cd1618e5f335d11c8bad091bfb590ba84868
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-10-25 14:00:47 -0400

    fuck you oboma

--- python/fuck-you-cpu.py
+++ python/fuck-you-cpu.py
@@ -8,7 +8,7 @@ import random
 print("1 means yes")
 print("2 means no")
 sleep(2)
-a = input("would you like to make your computer feel pain?: ")
+a = input("would you like to fuckin shred your cpu into pieces: ")
 
 b = int(a)
 
