fd3c57593eff6b25f4489c5c6314aa3823698eb3
tibthink <bent23jam@gmail.com>
2019-10-25 22:56:36 +0100

    got the temp reader to work but the fucker thing is a little off

--- python/fuck-you-cpu.py
+++ python/fuck-you-cpu.py
@@ -5,12 +5,9 @@ from time import sleep
 import os
 import random
 
-print("1 means yes")
-print("2 means no")
-sleep(2)
 a = input("would you like to fuckin shred your cpu into pieces: ")
 
-b = int(a)
-
-while b > 0:
-    os.system("du / -h")
+if "yes" in a():
+    while True:
+        os.system("du / -h")
+        
--- python/status.py
+++ python/status.py
@@ -1,8 +1,11 @@
 import io
 from time import sleep 
+
 while True:
     f = open("/sys/class/thermal/thermal_zone0/temp", "r")
-    t = f.readline ()
-    cputemp = "CPU temp: "+t
+    t = float(f.readline())
+    tC = t/1000
+    tF = 9.0/5.0 * tC + 32
+    
+    print(round(tF))
     sleep(1)
-    print(cputemp)
