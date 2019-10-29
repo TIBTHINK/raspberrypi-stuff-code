54024aefd126c7bb0c14125c9a70fdfc4d42bbe3
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-10-29 09:51:26 -0400

    Update status.py

--- python/status.py
+++ python/status.py
@@ -16,33 +16,39 @@ if "yes" in a:
                 GPIO.setwarnings(False)
                 GPIO.setmode(GPIO.BOARD)
                 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-                
+
                 f = open("/sys/class/thermal/thermal_zone0/temp", "r")
                 t = float(f.readline())
                 tC = t/1000
                 tF = 9.0/5.0 * tC + 32
-                
+
                 if tF > 112:
                     GPIO.output(8, GPIO.HIGH)
                     sleep(.1)
                     GPIO.output(8, GPIO.LOW
                     sleep(.1)
+
                 else:
                     GPIO.output(8, GPIO.LOW)
-    
-    
+
                 print(round(tF))
                 sleep(1)
-        else:
-            print("ok thats cool")
-            
-            f = open("/sys/class/thermal/thermal_zone0/temp", "r")
-                t = float(f.readline())
-                tC = t/1000
-                tF = 9.0/5.0 * tC + 32
-    
-    
-                print(round(tF))
-                sleep(1)
+
+
+
+            else:
+                print("ok thats cool")
+
+                f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+                    t = float(f.readline())
+                    tC = t/1000
+                    tF = 9.0/5.0 * tC + 32
+
+
+                    print(round(tF))
+                    sleep(1)
     else:
         print("sorry but this program will most likely not work on your pc")
+
+
+
