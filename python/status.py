3193baa10e738aa85f8134de46023e2a41e45c7c
tibthink <bent23jam@gmail.com>
2019-10-29 22:04:19 +0000

    ok error is gone

--- python/status.py
+++ python/status.py
@@ -1,17 +1,15 @@
-#doesnt work i have a syntax error on line 43
-import io
+# doesnt work i have a syntax error on line 43
 import os
 from time import sleep
 
 if os.geteuid() != 0:
-    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo !!' then run the script.\nExiting.")
+    exit(
+        "You need to have root privileges to run this script.\nPlease try again, this time using 'sudo !!' then run the script.\nExiting.")
 
 a = input("is this a raspberry pi?: ")
-b = input("do you have a led set up to this pi on pin 8?: ")
-
 if "yes" in a:
     while True:
-
+        b = input("do you have a led set up to this pi on pin 8?: ")
         if "yes" in b:
             while True:
 
@@ -23,8 +21,8 @@ if "yes" in a:
 
                 f = open("/sys/class/thermal/thermal_zone0/temp", "r")
                 t = float(f.readline())
-                tC = t/1000
-                tF = 9.0/5.0 * tC + 32
+                tC = t / 1000
+                tF = 9.0 / 5.0 * tC + 32
 
                 if tF > 112:
                     GPIO.output(8, GPIO.HIGH)
@@ -37,19 +35,15 @@ if "yes" in a:
 
                 print(round(tF))
                 sleep(1)
-
-
-
             else:
                 print("ok thats cool")
 
                 f = open("/sys/class/thermal/thermal_zone0/temp", "r")
-                    t = float(f.readline())
-                    tC = t/1000
-                    tF = 9.0/5.0 * tC + 32
+                t = float(f.readline())
+                tC = t / 1000
+                tF = 9.0 / 5.0 * tC + 32
 
-
-                    print(round(tF))
-                    sleep(1)
-    else:
-        print("sorry but this program will most likely not work on your pc")
+                print(round(tF))
+                sleep(1)
+else:
+    print("sorry but this program will most likely not work on your pc")
