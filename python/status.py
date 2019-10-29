72a53842b605a74dd1ed50514ea93a8f6fdc2cfe
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-10-29 09:14:14 -0400

    Update status.py

--- python/status.py
+++ python/status.py
@@ -1,26 +1,29 @@
 import io
-import RPi.GPIO as GPIO
-from time import sleep 
+from time import sleep
+
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo !!' then run the script.\nExiting.")
+
+a = input("is this a raspberry pi?: ")
+
+if "yes" in a:
+    while True:
+        import RPi.GPIO as GPIO
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+        while True:
+            f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+            t = float(f.readline())
+            tC = t/1000
+            tF = 9.0/5.0 * tC + 32
 
 
-GPIO.setwarnings(False) 
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-while True:
-    f = open("/sys/class/thermal/thermal_zone0/temp", "r")
-    t = float(f.readline())
-    tC = t/1000
-    tF = 9.0/5.0 * tC + 32
-
-    if tF > 112:
-        GPIO.output(8, GPIO.HIGH)
-        sleep(.1)
-        GPIO.output(8, GPIO.LOW)
-        sleep(.1)
-        
+            print(round(tF))
+            sleep(1)
     else:
-        GPIO.output(8, GPIO.LOW)
+        print("sorry but this program will most likely not work on your pc")
 
-    print(round(tF))
-    sleep(1)
-    
+
+
