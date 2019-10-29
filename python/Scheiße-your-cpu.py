e7836e7077def93bd675d1163662cdc25ad159d9
tibthink <bent23jam@gmail.com>
2019-10-29 08:32:06 -0400

    yeet

---
+++ python/Scheiße-your-cpu.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import time
+from time import sleep
+import os
+import random
+
+a = input("would you like to fuckin shred your cpu into pieces: ")
+
+if "yes" in a:
+    while True:
+        os.system("du / -h")
+else:
+    os.system("clear")
+    sleep(.1)
+    print("ok whore")
+    exit()
