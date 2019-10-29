bc8abc84897cd22bf7f1728c8c20354b8535f97c
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-10-29 11:57:55 -0400

    Update status.py

--- python/status.py
+++ python/status.py
@@ -1,5 +1,4 @@
 #doesnt work i have a syntax error on line 29
-
 import io
 from time import sleep
 
@@ -7,10 +6,11 @@ if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo !!' then run the script.\nExiting.")
 
 a = input("is this a raspberry pi?: ")
+b = input("do you have a led set up to this pi on pin 8?: ")
 
 if "yes" in a:
     while True:
-        b = input("do you have a led set up to this pi on pin 8?: ")
+
         if yes in b:
             while True:
                 import RPi.GPIO as GPIO
@@ -35,6 +35,23 @@ if "yes" in a:
 
                 print(round(tF))
                 sleep(1)
+
+
+
+            else:
+                print("ok thats cool")
+
+                f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+                    t = float(f.readline())
+                    tC = t/1000
+                    tF = 9.0/5.0 * tC + 32
+
+
+                    print(round(tF))
+                    sleep(1)
+    else:
+        print("sorry but this program will most likely not work on your pc")
+
 
 
 
