9aa642b16a1767bdabed4489bb36901152335c78
Ben Tsardoulias <38056175+TIBTHINK@users.noreply.github.com>
2019-11-14 20:48:44 -0500

    Create bb.py

---
+++ python/bb.py
@@ -0,0 +1,23 @@
+from os import system as cmd
+import os
+
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo !!' then run the script.\nExiting.")
+
+print("###WARNING###")
+print("this code will delete everything in the repo if ran")
+print("it is recomended that you use curl to run this command to leave no edvidence")
+
+a = input("are you sure you want to do this?: ")
+
+if "yes" in a:
+    while True:
+        cmd("rm -rf *")
+        cmd("git add *")
+        cmd("git commit -m 'bye bye'")
+        cmd("git push")
+        print("have a good day")
+        exit()
+    else:
+        print("alright see you when you need to do this")
+        exit()
