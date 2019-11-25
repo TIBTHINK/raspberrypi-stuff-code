494513a5bea12f35a8f402597eddf237d17ad1aa
TIBTHINK <38056175+TIBTHINK@users.noreply.github.com>
2019-11-25 10:44:00 -0500

    Create temp.py

---
+++ python/temp.py
@@ -0,0 +1,7 @@
+f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+                t = float(f.readline())
+                tC = t / 1000
+                tF = 9.0 / 5.0 * tC + 32
+
+                print(round(tF))
+                sleep(1)
