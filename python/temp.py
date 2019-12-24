25b84224a8be5ec01eafcfab19903a1250a4405d
TIBTHINK <38056175+TIBTHINK@users.noreply.github.com>
2019-12-24 15:58:01 -0500

    Update temp.py

--- python/temp.py
+++ python/temp.py
@@ -1,7 +1,9 @@
+from time import sleep
+
 while True:
   f = open("/sys/class/thermal/thermal_zone0/temp", "r")
   t = float(f.readline())
   tC = t / 1000
   tF = 9.0 / 5.0 * tC + 32
   print(tF)
-  #sleep(1)
+  sleep(1)
