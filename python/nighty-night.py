dbbf0a41082c2869fc52eafef2bf9d391067dee4
TIBTHINK <38056175+TIBTHINK@users.noreply.github.com>
2020-01-20 06:38:38 -0500

    crashes my main server after 15 seconds of running

---
+++ python/nighty-night.py
@@ -0,0 +1,20 @@
+import decimal
+
+def pi():
+    
+    decimal.getcontext().prec += 2  # extra digits for intermediate steps
+    three = decimal.Decimal(3)      # substitute "three=3.0" for regular floats
+    lasts, t, s, n, na, d, da = 0, three, 3, 1, 0, 0, 24
+    while s != lasts:
+        lasts = s
+        n, na = n + na, na + 8
+        d, da = d + da, da + 32
+        t = (t * n) / d
+        s += t
+    decimal.getcontext().prec -= 2
+    return +s               # unary plus applies the new precision
+
+decimal.getcontext().prec = 5**14
+pi = pi()
+
+print(pi)
